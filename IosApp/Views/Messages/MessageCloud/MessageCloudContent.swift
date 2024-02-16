bb5d9e71de3318a005c64ab06df43f43bdd28e7d
 Владислав <vladislav@MacBook-Pro-Vladislav.local>
2024-02-16 12:21:42 +0300

    Начата работа над вложениями фото

--- IosApp/Views/Messages/MessageCloud/MessageCloudContent.swift
+++ IosApp/Views/Messages/MessageCloud/MessageCloudContent.swift
@@ -1,4 +1,6 @@
 import SwiftUI
+import SDWebImage
+import SDWebImageSwiftUI
 
 struct MessageCloudContent: View {
     var isSticker: Bool;
@@ -7,20 +9,49 @@ struct MessageCloudContent: View {
     var lastSticker: Sticker?;
     var time: String;
     
+    func createSubarraysArray<T>(arr: [T], byNumber: Int) -> [[T]]{
+        var array: [[T]] = [];
+        
+        for i in stride(from: 0, to: arr.count, by: byNumber + 1){
+            if(i + byNumber >= arr.count - 1){
+                array.append(Array(arr[i...arr.count - 1]));
+                break;
+            }
+            array.append(Array(arr[i...i + byNumber]));
+        }
+        
+        return array;
+    }
+    
     var body: some View {
         Group{
             VStack{
-                if(message.attachments.count > 0){
-                    if(isSticker){
-                        if(!isAnimatedSticker){
-                            MessageSticker(url: lastSticker?.images.last?.url);
-                        }
-                        else{
-                            AnimatedMessageSticker(sticker: lastSticker!)
+                if(isSticker){
+                    if(!isAnimatedSticker){
+                        MessageSticker(url: lastSticker?.images.last?.url);
+                    }
+                    else{
+                        AnimatedMessageSticker(sticker: lastSticker!)
+                    }
+                } else{
+                    if(!message.attachments.isEmpty){
+                        let messageRows = createSubarraysArray(arr: message.attachments, byNumber: 1);
+                        Grid{
+                            ForEach(0..<messageRows.count, id: \.self){ i in
+                                let attachmentRow = messageRows[i];
+                                GridRow{
+                                    ForEach(0..<attachmentRow.count, id: \.self){ attachmentItem in
+                                        let attachment = attachmentRow[attachmentItem];
+                                        if(attachment.type == "photo"){
+                                            WebImage(url: URL(string: attachment.photo!.sizes.last!.url))
+                                                .resizable()
+                                                .scaledToFill()
+                                        }
+                                    }
+                                }
+                            }
                         }
                     }
-                }
-                if(!isSticker){
                     Text(message.text)
                         .font(.system(size: 17))
                         .foregroundColor(.white)
--- IosApp/Views/Messages/MessageSticker/AnimatedMessageSticker.swift
+++ IosApp/Views/Messages/MessageSticker/AnimatedMessageSticker.swift
@@ -4,6 +4,7 @@ struct AnimatedMessageSticker: View{
     var sticker: Sticker;
     
     // TODO: Добавить кэширование анимированных стикеров
+    // TODO: При скролле остановить анимацию
     
     var body: some View{
         LottieView(filePath: sticker.animationUrl ?? "", loop: true)
